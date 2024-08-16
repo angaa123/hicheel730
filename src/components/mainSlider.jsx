7c1a4672ba8d2b6f86818fc6dc28a7b2277e806b
angaa123 <75901266+angaa123@users.noreply.github.com>
2024-08-16 10:30:49 +0800

    top4

--- src/components/mainSlider.jsx
+++ src/components/mainSlider.jsx
@@ -1,29 +1,66 @@
 import Link from "next/link";
 import React from "react";
-const mockImg =
-	"https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n7UvFitaCT~CzZ5DqyKpCUoCHGwheEv9NNQTzNAOO9CTpKhBNgqDw1hKKxE-rchvhmoiPCedKAjkn93IifesRTTGVeaRptLzbbfDNffKbYju9Eka9JYFz22tod0-GwbzO205dw9x5TjvZ7X0vzMl28u8GkII6K7980NMnN0NV8zSFhiJtj9A8RqwUvaHc7LWgH1AjJTpFKG~NeQeP5CQGnxEsZm~KfMQBMFLVA7qEvajjG~6b8JuJrawqM2Gj~m3~MgcYkwzjkRQUO1bER~ug~Oa5zxgKghSkR2b0X92zH2kYUwBG4Q2hwxQ6piJv5nhtSVLdny6zvck3wMnYU68zg__";
-
+const baseURL = "https://dev.to/api/articles?&per_page=4&top=4";
 const MainSlider = () => {
+	const [articles, setArticles] = React.useState([]);
+	const [loading, setLoading] = React.useState(true);
+	const [error, setError] = React.useState(false);
+
+	React.useEffect(() => {
+		async function fetchArticles() {
+			try {
+				const response = await fetch(baseURL);
+				console.log(response);
+				const json = await response.json();
+				console.log(json);
+				setArticles(json);
+			} catch (error) {
+				console.log(error);
+				setError(true);
+			}
+			setLoading(false);
+		}
+
+		fetchArticles();
+	}, []);
+
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>Error!</div>;
+
 	return (
 		<div className="hidden md:flex w-full justify-center ">
-			<div className="relative w-5/6 ">
-				<div
-					className="relative bg-cover bg-center bg-no-repeat p-8 h-[800px] w-full rounded-3xl"
-					style={{ backgroundImage: `url(${mockImg})` }}
-				>
-					<div className="absolute bottom-2 left-2 rounded-md bg-white  h-max w-1/3  m-4 ">
-						<div className=" m-10 flex flex-col gap-4">
-							<p className=" bg-indigo-700 w-max border py-2 px-4 text-white text-sm font-medium rounded-xl">
-								Technology
-							</p>
-							<h1 className=" text-4xl font-bold text-gray-900">
-								Grid system for better Design User Interface
-							</h1>
-						</div>
+			<div className=" w-5/6 flex overflow-hidden">
+				{articles.map((article) => (
+					<div className="min-w-full n">
+						<Link
+							className="w-full relative"
+							key={article.id}
+							href={`/articles/${article.id}`}
+						>
+							<div
+								className="relative bg-cover bg-center bg-no-repeat p-8 h-[800px] w-full rounded-3xl"
+								style={{ backgroundImage: `url(${article.cover_image})` }}
+							>
+								<div className="absolute bottom-2 left-2 rounded-md bg-white  h-max w-1/3  m-4 ">
+									<div className=" m-10 flex flex-col gap-4">
+										<p>
+											{article.tag_list.map((tag) => (
+												<span className=" bg-indigo-700 w-max border py-2 px-4 text-white text-sm font-medium rounded-xl ">
+													{tag}{" "}
+												</span>
+											))}
+										</p>
+										<h1 className=" text-4xl font-bold text-gray-900">
+											{article.title}
+										</h1>
+									</div>
 
-						<p className=" m-10">August 20, 2022</p>
+									<p className=" m-10">{article.readable_publish_date}</p>
+								</div>
+							</div>
+						</Link>
 					</div>
-				</div>
+				))}
 				<div className="felx  w-full ">
 					<h1 className=""></h1>
 				</div>
